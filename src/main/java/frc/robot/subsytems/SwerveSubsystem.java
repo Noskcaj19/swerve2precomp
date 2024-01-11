5f61a204e6766ba3b90e6473757622c361204910
10560 <wren.moyer@protonmail.com>
2024-01-10 19:34:10 -0500

    yahoo

--- src/main/java/frc/robot/subsytems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsytems/SwerveSubsystem.java
@@ -19,34 +19,30 @@ public class SwerveSubsystem extends SubsystemBase {
     private final Translation2d backLeftLocation = new Translation2d(-0.381, 0.381);
     private final Translation2d backRightLocation = new Translation2d(-0.381, -0.381);
 
-    private final SwerveModule fLSwerve = new SwerveModule(0,0,0, false, false, Rotation2d.fromRadians(1.5754));
-    private final SwerveModule fRSwerve = new SwerveModule(0,0,0,false ,false, Rotation2d.fromRadians(-1.2026));
-    private final SwerveModule bLSwerve = new SwerveModule(0,0,0, false, false, Rotation2d.fromRadians(-2.6982));
-    private final SwerveModule bRSwerve = new SwerveModule(0,0,0,false,false, Rotation2d.fromRadians(2.6952));
+    private final SwerveModule fLSwerve = new SwerveModule(0, 0, 0, false, false, Rotation2d.fromRadians(1.5754));
+    private final SwerveModule fRSwerve = new SwerveModule(0, 0, 0, false, false, Rotation2d.fromRadians(-1.2026));
+    private final SwerveModule bLSwerve = new SwerveModule(0, 0, 0, false, false, Rotation2d.fromRadians(-2.6982));
+    private final SwerveModule bRSwerve = new SwerveModule(0, 0, 0, false, false, Rotation2d.fromRadians(2.6952));
 
     XboxController primaryController = new XboxController(0);
     XboxController secondaryController = new XboxController(0);
 
     private final AHRS gyro = new AHRS(SPI.Port.kMXP);
 
-    private final SwerveDriveKinematics kinematics =
-            new SwerveDriveKinematics(
-                    frontLeftLocation, frontRightLocation, backLeftLocation, backRightLocation);
+    private final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+            frontLeftLocation, frontRightLocation, backLeftLocation, backRightLocation);
 
     public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
-        var swerveModuleStates =
-            kinematics.toSwerveModuleStates(
+        var swerveModuleStates = kinematics.toSwerveModuleStates(
                 fieldRelative
-                    ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, gyro.getRotation2d())
-                    : new ChassisSpeeds(xSpeed, ySpeed, rot));
-        //TODO: DEFINE MAX SPEED
+                        ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, gyro.getRotation2d())
+                        : new ChassisSpeeds(xSpeed, ySpeed, rot));
+        // TODO: DEFINE MAX SPEED
         SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, MaxSpeed);
         fLSwerve.setDesiredState(swerveModuleStates[0]);
         fRSwerve.setDesiredState(swerveModuleStates[1]);
         bLSwerve.setDesiredState(swerveModuleStates[2]);
         bRSwerve.setDesiredState(swerveModuleStates[3]);
-        }
-
-    
+    }
 
 }
