aa4725ad0952772aedd601020fafae9bb23ec4bb
abigal3n <abbykittygaming13@gmail.com>
2024-01-20 14:35:04 -0500

    hhj

--- src/main/java/frc/robot/command/fetch.java
+++ src/main/java/frc/robot/command/fetch.java
@@ -27,16 +27,33 @@ public class fetch {
   0.0,
   0,
   new
-  TrapezoidProfile.Constraints(Constants.DriveConstants.kMaxVelocityMetersPerSecond
+  TrapezoidProfile.Constraints(Constants.DriveConstants.MaxVelocityMetersPerSecond
   / 4, 2.5));
   private ProfiledPIDController pidX = new ProfiledPIDController(
   2,
   0.0,
   0,
   new
-  TrapezoidProfile.Constraints(Constants.DriveConstants.kMaxVelocityMetersPerSecond
+  TrapezoidProfile.Constraints(Constants.DriveConstants.MaxVelocityMetersPerSecond
   / 4, 2.5));
-    private static pose3d getTagPos() {
 
+private ProfiledPIDController pidTheta = new ProfiledPIDController(
+      .06,
+      0.0,
+      0,
+      new TrapezoidProfile.Constraints(Constants.DriveConstants.MaxAngularVelocityRadiansPerSecond / 4, 1.5));
+
+    
+    private static Pose3d getTagPose() {
+        var pose = LimelightHelpers.getTargetPose3d_RobotSpace("");
+        return new Pose3d(pose[0], -pose[1], pose[2], new Rotation3d(pose[3], pose[4], pose[5]))
     }
 }
+
+
+//plan: robot looks for note and faces it
+//you throw the note and it turns around to find it
+//it looks at it
+//when button is pushed it goes to it
+//when another button is pushed it picks it up
+//then you can push a button to have it retrace its steps it took to go to the 
