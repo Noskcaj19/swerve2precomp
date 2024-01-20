4be1e80792e37450eb62d614133518ed3ac3688d
10560 <wren.moyer@protonmail.com>
2024-01-20 14:34:16 -0500

    lighting limes

---
+++ src/main/java/frc/robot/command/LimelightlightDrive.java
@@ -0,0 +1,24 @@
+package frc.robot.command;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.subsytems.SwerveSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class LimelightlightDrive extends Command {
+
+    private SwerveSubsystem swerveSub;
+    private final PIDController turnPID;
+    private final PIDController distancePID;
+    private Limelight limelight;
+
+    public LimelightlightDrive(SwerveSubsystem swerveSub, XboxController primaryController, Limelight limelight) {
+
+        addRequirements(swerveSub);
+        this.swerveSub = swerveSub;
+        this.limelight = 
+
+    }
+
+}
