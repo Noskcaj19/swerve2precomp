0793dd54455cbeded30b1eb95c2125ee28e3f745
abigal3n <abbykittygaming13@gmail.com>
2024-02-03 11:13:53 -0500

    umuh

--- src/main/java/frc/robot/subsytems/SwerveModule.java
+++ src/main/java/frc/robot/subsytems/SwerveModule.java
@@ -128,7 +128,7 @@ public class SwerveModule {
 
                 driveEncoder = driveMotor.getEncoder();
                 // TODO: FIX CONSTANTS
-                double positionConversionFactor = Math.PI * Constants.ModuleType.getWheelDiameter()
+        double positionConversionFactor = Math.PI * Constants.ModuleType.getWheelDiameter()
                                 * Constants.ModuleType.getDriveReduction();
                 driveEncoder.setPositionConversionFactor(positionConversionFactor);
                 driveEncoder.setVelocityConversionFactor(positionConversionFactor / 60);
@@ -153,14 +153,14 @@ public class SwerveModule {
                 // pidController.setFF(1.534);
                 pidController.setOutputRange(-1, 1);
 
-                    double currentAngleRadiansMod = integratedTurningEncoder.getPosition() % (2.0 * Math.PI);
+                    double currentAngleRadiansMod = turningEncoder.getPosition() % (2.0 * Math.PI);
     if (currentAngleRadiansMod < 0.0) {
       currentAngleRadiansMod += 2.0 * Math.PI;
     }
 
     // The reference angle has the range [0, 2pi) but the Neo's encoder can go above
     // that
-    double adjustedReferenceAngleRadians = optimizedState.angle.getRadians() + m_integratedTurningEncoder.getPosition()
+    double adjustedReferenceAngleRadians = optimizedState.angle.getRadians() + turningEncoder.getPosition()
         - currentAngleRadiansMod;
     if (optimizedState.angle.getRadians() - currentAngleRadiansMod > Math.PI) {
       adjustedReferenceAngleRadians -= 2.0 * Math.PI;
