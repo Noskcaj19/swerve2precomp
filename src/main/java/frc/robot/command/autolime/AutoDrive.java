b786ba5eb74e45709b650f6869ecdcbbdea8ef77
abigal3n <abbykittygaming13@gmail.com>
2024-02-03 15:54:15 -0500

    Co-authored-by: mirabellek <mirabellek@users.noreply.github.com>

---
+++ src/main/java/frc/robot/command/autolime/AutoDrive.java
@@ -0,0 +1,27 @@
+package frc.robot.command.autolime;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+// import frc.robot.subsystems.Drivetrain;
+
+public class AutoDrive extends Command {
+
+    private SwerveSubsystem swerveSub;
+
+    public AutoDrive(SwerveSubsystem swerveSub) {
+
+        this.swerveSub = swerveSub;
+     
+}package frc.robot.command.autolime;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+// import frc.robot.subsystems.Drivetrain;
+
+public class AutoDrive extends CommandBase {
+
+    private SwerveSubsystem swerveSub;
+
+    public BalanceAuto(Drivetrain drivetrain, double speed) {
+
+        this.swerveSub = swerveSub;
+        
+}    }
--- src/main/java/frc/robot/subsytems/SwerveModule.java
+++ src/main/java/frc/robot/subsytems/SwerveModule.java
@@ -197,3 +197,13 @@ public class SwerveModule {
                 driveEncoder.setPosition(0);
         }
 }
+
+//but im a CounterClockwise_Positive
+//im a weirdoOoooo
+//what the hell am i doing here
+//i dont belong here
+// //oOoO
+// shEEEeeeees
+// rUnning out of TIIIMmeeee
+// shEEessss
+// rUUning out of TimETIMETIMETIMEEEEEEEEEeeeee
