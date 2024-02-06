5b1c8161f8c21ca0885f7956a300b65c257a8450
abigal3n <abbykittygaming13@gmail.com>
2024-02-05 20:07:05 -0500

    no more feedforward :(

--- src/main/java/frc/robot/subsytems/SwerveModule.java
+++ src/main/java/frc/robot/subsytems/SwerveModule.java
@@ -180,10 +180,10 @@ public class SwerveModule {
 
         public void setDesiredState(SwerveModuleState desiredState) {
                 // Optimize the reference state to avoid spinning further than 90 degrees
-                // SwerveModuleState state = optimizeModuleState(desiredState);
-                SwerveModuleState state = desiredState;
+                SwerveModuleState state = optimizeModuleState(desiredState);
+                // SwerveModuleState state = desiredState;
 
-                var fff = feedforward.calculate(desiredState.speedMetersPerSecond);
+                var fff =0;// feedforward.calculate(desiredState.speedMetersPerSecond);
                 System.out.println(fff);
 
                 // Calculate the turning motor output from the turning PID controller.
