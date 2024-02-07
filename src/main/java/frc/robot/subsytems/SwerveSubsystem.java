91c29f709730aa0ac78d3497f048d1e2071ce5ef
abigal3n <abbykittygaming13@gmail.com>
2024-02-07 15:05:32 -0500

    pathplanneralmostsetup:(

--- src/main/java/frc/robot/subsytems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsytems/SwerveSubsystem.java
@@ -95,13 +95,17 @@ public class SwerveSubsystem extends SubsystemBase {
             AutoBuilder.configureHolonomic(
             this::getPose, // Robot pose supplier
             this::resetOmetry, // Method to reset odometry (will be called if your auto has a starting pose)
-            this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-            this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+            this::getSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+             (ChassisSpeeds speeds) -> {
+                                swerveModuleStates = DriveConstants.kinematics.toSwerveModuleStates(
+                                                 ChassisSpeeds.discretize(speeds, .02));
+                                driveStates(swerveModuleStates);
+                },// Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
             new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-                    new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
-                    new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
-                    4.5, // Max module speed, in m/s
-                    0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+                    new PIDConstants(9.5, 0.0, 0.0), // Translation PID constants
+                    new PIDConstants(9, 0.0, 0.0), // Rotation PID constants
+                    1.5, // Max module speed, in m/s
+                    0.3, // Drive base radius in meters. Distance from robot center to furthest module.
                     new ReplanningConfig() // Default path replanning config. See the API for the options here
             ),
             () -> {
@@ -178,8 +182,23 @@ public class SwerveSubsystem extends SubsystemBase {
     return states;
   }
 
+  
+
     public ChassisSpeeds getSpeeds() {
         return DriveConstants.kinematics.toChassisSpeeds(getModuleStates());
     }
 
-}
+        public void driveStates(SwerveModuleState[] swerveModuleStates) {
+        SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates,
+                Constants.DriveConstants.MaxVelocityMetersPerSecond);
+
+         SwerveModuleState[] optimizedSwerveModuleStates = {
+                fLSwerve.optimizeModuleState(swerveModuleStates[0]),
+                fRSwerve.optimizeModuleState(swerveModuleStates[1]),
+                bLSwerve.optimizeModuleState(swerveModuleStates[2]),
+                bRSwerve.optimizeModuleState(swerveModuleStates[3]),
+         };
+        }
+    }
+
+
